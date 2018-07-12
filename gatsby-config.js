6cdffdc08a5e8988dc3206ee8eb9c3cd3a8537ed
danklut <danklutmeyer@hotmail.ca>
2018-07-12 05:36:01 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Forty by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Forty by HTML5 UP"
+    title: "AOM Cloud Enterprise",
+    author: "danklut",
+    description: "An Affiliate Online Marketing Platform that teaches you the Tips and Tricks of Online Marketing."
   },
   pathPrefix: '/',
   plugins: [
