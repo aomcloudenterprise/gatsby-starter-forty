ae0d98569969c681c510fdaffd3c3edee85b5ad1
danklut <danklutmeyer@hotmail.ca>
2018-07-12 05:42:14 -0700

    Update landing.js

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -10,7 +10,7 @@ import pic10 from '../assets/images/pic10.jpg'
 const Landing = (props) => (
     <div>
         <Helmet>
-            <title>Landing - Forty by HTML5 UP</title>
+            <title>AOM Cloud Enterprise</title>
             <meta name="description" content="Landing Page" />
         </Helmet>
 
